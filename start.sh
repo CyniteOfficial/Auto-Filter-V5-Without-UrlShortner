49a1f0dcb9526c6e1b4283ef9274b2ffe000cf94
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-01-04 09:09:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5.git /Auto-Filter-V5
+  git clone https://github.com/Cyniteofficial/Auto-Filter-V5-Without-UrlShortner.git /Auto-Filter-V5-Without-UrlShortner
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5
+  git clone $UPSTREAM_REPO /Auto-Filter-V5-Without-UrlShortner
 fi
 cd /Auto-Filter-V5
 pip3 install -U -r requirements.txt
